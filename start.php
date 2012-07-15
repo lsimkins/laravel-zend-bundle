26214636a3318cdb1001a7c9c8e2f5111b8b55a2
lsimkins <lucas.simkins@gmail.com>
2012-07-15 09:32:30 -0500

    Updated start.php docblock to reference license.

--- start.php
+++ start.php
@@ -1,10 +1,23 @@
 <?php
 
+/**
+ * This file is a part of Zend Framework.
+ * Copyright (c) 2005-2010, Zend Technologies USA, Inc.
+ * All rights reserved.
+ * 
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * Reference Zend Framework Performance Guide at:
+ * http://framework.zend.com/manual/1.11/en/performance.classloading.html
+ */
 set_include_path(implode(PATH_SEPARATOR, array(
     __DIR__ . '/library',
     get_include_path(),
 )));
 
- require_once Bundle::path('zend') . '/library/Zend/Loader/Autoloader.php';
+Bundle::path('zend') . '/library/Zend/Loader/Autoloader.php';
 
 $autoloader = Zend_Loader_Autoloader::getInstance();
